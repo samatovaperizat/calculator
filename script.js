8b6474424d07f79b145308f7c959b24c42a85b00
Perizat <samatovaperizat@gmail.com>
2022-05-24 22:37:54 +0200

    now, the app is working

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const display = document.querySelector("#display");
-const button = document.querySelector("button");
+const buttons = document.querySelectorAll("button");
 
 buttons.forEach((item) => {
     item.onclick = () => {
